caaf2e275ce3f6a34565f79498e726a71004f2e0
Kyle Hodgson <kyle.hodgson@gmail.com>
2015-11-28 23:51:26 -0500

    adding new mobile project

---
+++ BarsMobile/platforms/android/assets/www/TwitterService.js
@@ -0,0 +1,12 @@
+BattleTapez = typeof BattleTapez === "undefined" ? {} : BattleTapez;
+
+BattleTapez.TwitterService = function() {
+    //$twitterApi.configure('oa2667DihorG840pIHZYEA', 'N3d5E3C0guHodgGPS0yeu8559LzmAdfrKm7eERky4', '82783590-VP8O1ZeQTioq47qQZvEM4fsr9JJ3T8O8okIgWrkup');
+    return {
+
+        postStatus: function(message) {
+
+            return "SUCCESS";
+        }
+    }
+};
---
+++ BarsMobile/platforms/android/assets/www/app.js
@@ -0,0 +1,7 @@
+BattleTapez = typeof BattleTapez === "undefined" ? {} : BattleTapez;
+
+BattleTapez.BARS = angular.module("bars", ['ngAnimate']);
+BattleTapez.BARS.factory("Battle", BattleTapez.BattleService);
+BattleTapez.BARS.factory("Twitter", BattleTapez.TwitterService);
+BattleTapez.BARS.controller("mobileCtrl", BattleTapez.MobileController);
+BattleTapez.BARS.directive("performanceScores", BattleTapez.PerformanceScoresDirective);
