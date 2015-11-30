a66e7431766513fa71f2d089c59957f5788d17a1
Kyle Hodgson <kyle.hodgson@gmail.com>
2015-11-30 00:26:43 -0500

    fixing bug in the how to score page

--- src/www/app/app.js
+++ src/www/app/app.js
@@ -5,4 +5,4 @@ BattleTapez.BARS.factory("Battle", BattleTapez.BattleService);
 BattleTapez.BARS.factory("Twitter", BattleTapez.TwitterService);
 BattleTapez.BARS.controller("mobileCtrl", BattleTapez.MobileController);
 BattleTapez.BARS.directive("performanceScores", BattleTapez.PerformanceScoresDirective);
-BattleTapez.BARS.directive("howToScore", BattleTapez.HowToScoreDirective);
+BattleTapez.BARS.directive("helpText", BattleTapez.HowToScoreDirective);
