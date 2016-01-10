7af3f2e07e1b1c5546ed7794cc97136ddd760407
Kyle Hodgson <kyle.hodgson@gmail.com>
2016-01-10 12:32:29 -0500

    functional specs working

--- specs/functional/spec.js
+++ specs/functional/spec.js
@@ -1,7 +1,8 @@
+var EC = protractor.ExpectedConditions
+
 describe('B.A.R.S.', function() {
-    
-    var page;
-    var EC = protractor.ExpectedConditions
+    browser.ignoreSynchronization = true;
+    var page;    
 
     beforeEach(function () {
         page = new BarsPage(EC);
@@ -9,43 +10,42 @@ describe('B.A.R.S.', function() {
   
     it('should show instructions for scoring', function() {
         page.viewInstructionsButton.click().then(function() {
-            browser.wait(EC.visibilityOf(page.scoreInstructionsDiv),5000)
             expect(page.scoreInstructionsDiv.getText()).toMatch("Once the rappers names have been entered")
         })
         
     });
     
-    it('should allow judges to enter the rappers for a battle',function() {
+    it('should allow judges to score a battle',function() {
         page.startBattle.click()
         page.enterRapperOne('rapper one')
-        
         expect(page.rapperOneButton.getText()).toMatch('rapper one')
-    //     page.enterRapperOne('rapper one')
-    //     page.enterRapperTwo('rapper two')
-    //     page.rapperTwoButton.click()
-    //     expect(page.text).toMatch("Scoring round for rapper two")
+        
+        page.enterRapperTwo('rapper two')
+        page.rapperTwoButton.click()
+        expect(page.punchScoringTitle.getText()).toMatch('Scoring punches for rapper two round 1')
+        
+        page.punchButton.click()
+        page.punchButton.click()
+        expect(page.punchTally.getText()).toMatch('2')
+        page.roundFinishedButton.click()
+        
+        page.categoryScore('jokes',4).click()
+        page.categoryScore('rapping',5).click()
+        page.categoryScore('errors',1).click()
+        expect(page.errorTally.getText()).toMatch('Subtracting -1 points for errors')
+        
+        page.nextRoundButton.click()
+        page.punchButton.click()
+        page.punchButton.click()
+        page.punchButton.click()
+        page.punchButton.click()
+        page.roundFinishedButton.click()
+        page.categoryScore('antics',2).click()
+        page.categoryScore('freshness',3).click()
+        page.finishBattleButton.click()
+        
+        expect(page.winner.getText()).toMatch('Winner: rapper two!')
     });
-    
-    // it('should allow judges to score a battle',function() {
-    //     page.enterRapperOne('rapper one')
-    //     page.enterRapperTwo('rapper two')
-    //     page.rapperOneButton.click()
-        
-    //     page.punchButton.click()
-    //     page.punchButton.click()
-    //     page.punchButton.click()
-    //     page.nextRoundButton.click()
-    //     page.category('jokes').stars(1).click()
-    //     page.nextRoundButton.click()
-    //     page.punchButton.click()
-    //     page.punchButton.click()
-    //     page.nextRoundButton.click()
-    //     page.category('jokes').stars(1).click()
-        
-    //     page.finishBattleButton.click()
-    //     expect(page.winner).toBe('rapper one')
-        
-    // })
 });
 
 
@@ -53,51 +53,32 @@ var BarsPage = function () {
     browser.get('http://localhost:8000');
 };
 
+var waitForElement = function(element)  {
+    browser.wait(EC.visibilityOf(element),5000)
+    return element
+}
+
 BarsPage.prototype = Object.create({}, {
-    viewInstructionsButton: { 
-        get: function() { 
-            return $('#btn-how-to-score')
-        }
-    },
-    scoreInstructionsDiv: { 
-        get: function() { 
-            return $('#text-how-to-score')
-        } 
-    },
-    startBattle: {
-        get: function() {
-            return $('#btn-start-battle')
-        }
-    },
-    rapperOneField: { 
-        get: function() {
-            return element(by.model('rapper1Name'))
-        }
-    },
-    rapperTwoField: {
-        get: function() {
-            return element(by.model('rapper1Name'))
-        }
-    },
-    rapperOneButton: {
-        get: function() {
-            return $('#btn-r1-start')
-        }
-    },
-    rapperTwoButton: {
-        get: function() {
-            return $('#btn-r2-start')
-        }
-    },
-    enterRapperOne: {
-        value: function(keys) {
-            var promise = this.rapperOneField.sendKeys(keys)
-            return promise;
-        }
-    },
-    enterRapperTwo: {
-        value: function(keys) {
-            return this.rapperTwoField.sendKeys(keys)
-        }
-    }
-  });
+    punchScoringTitle:      { get: function() { return waitForElement( $('#punch-scoring-title'))}},
+    viewInstructionsButton: { get: function() { return waitForElement( $('#btn-how-to-score'))}},
+    scoreInstructionsDiv:   { get: function() { return waitForElement( $('#text-how-to-score'))}},
+    startBattle:            { get: function() { return waitForElement( $('#btn-start-battle'))}},
+    rapperOneButton:        { get: function() { return waitForElement( $('#btn-r1-start'))}},
+    rapperTwoButton:        { get: function() { return waitForElement( $('#btn-r2-start'))}},
+    punchButton:            { get: function() { return waitForElement( $('#btnPunch'))}},
+    punchTally:             { get: function() { return waitForElement( $('#punch-tally'))}},
+    roundFinishedButton:    { get: function() { return waitForElement( $('#btn-round-finished'))}},
+    nextRoundButton:        { get: function() { return waitForElement( $('#btn-next-round'))}},
+    errorTally:             { get: function() { return waitForElement( $('#error-tally'))}},
+    finishBattleButton:     { get: function() { return waitForElement( $('#btn-finish-battle'))}},
+    winner:                 { get: function() { return waitForElement( $('#text-winner'))}},
+    rapperOneField:         { get: function() { return waitForElement(element(by.model('rapper1Name')))}},
+    rapperTwoField:         { get: function() { return waitForElement(element(by.model('rapper2Name')))}},
+    enterRapperOne:         { value: function(keys) { return this.rapperOneField.sendKeys(keys)}},
+    enterRapperTwo:         { value: function(keys) { return this.rapperTwoField.sendKeys(keys)}}
+});
+
+BarsPage.prototype.categoryScore = function (categoryName,score) {
+    return waitForElement(  $( "#" + categoryName + "-" + score) )
+}
+
